1677fa665405e9a4996bed77cd107469ed4133e1
koshaughnesseyRye <katie_oshaughnessey@rycountryday.org>
2019-12-08 14:46:35 -0500

    Adding some committed work here.

--- p5/dataTesting/sketch.js
+++ p5/dataTesting/sketch.js
@@ -1,7 +1,65 @@
+var canvas;
+var h1;
+var p;
+var x = 100;
+var y = 100;
+var bgcolor;
+var b;
+var b2;
+var on;
+var slid;
+var diam;
+var input;
+var nameP;
+
 function setup() {
-  // put setup code here
+  p = createP("I am developing web skills");
+  canvas = createCanvas(200, 200);
+  createP('');
+  slid = createSlider(10, 50, 30);
+  nameP = createP('Name');
+  b = createButton("press me");
+  b2 = createButton("button 2");
+  bgcolor = color(200);
+  b2.mousePressed(boxAppear);
+  b.mousePressed(changeColor);
+  diam = slid.value();
+  createP('');
+  input = createInput('type your name');
+  //h1 = createElement('h1', 'Waiting');
+  //h1.position(400, 600)
 }
 
 function draw() {
-  // put drawing code here
+  //clear();
+  background(bgcolor);
+  diam = slid.value();
+  fill(255, 0, 0);
+  ellipse(x, y, diam, diam);
+  x = x + random(-5, 5);
+  //h1.position(x, y);
+  if (on) {
+    fill(0, 255, 0);
+    stroke(255);
+    text("clicked!", 25, 25);
+    ellipse(0, 0, 25, 25);
+    rect(25, 25, 50, 20);
+  }
+  nameP.html(input.value());
+  text(input.value(), 10, 20);
+}
+
+function mousePressed() {
+  //changeColor();
+}
+
+function boxAppear() {
+  on = true;
+
+}
+
+// make button a callback 
+function changeColor() {
+  bgcolor = color(255, 0, 255);
+
 }
